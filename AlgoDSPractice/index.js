aefec95adf231b97cf01fc15bb681a24cf102042
Nikita <nikita_jain@apple.com>
2019-02-08 11:31:15 -0800

    AlgoDSPractice initial checkin

---
+++ AlgoDSPractice/index.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded',function(event){
+    console.log('Dom Loaded');
+    binarySearch();
+});
+
+var binarySearch = () => {
+    console.log("Binary search called");
+}
